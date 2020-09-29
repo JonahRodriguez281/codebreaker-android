7cb37394931ce7e350e483139e445abef0900908
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-09-29 09:42:08 -0600

    Added Application class and organized w/ MVC.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,5 +1,7 @@
-package edu.cnm.deepdive.codebreaker;
+package edu.cnm.deepdive.codebreaker.controller;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -7,8 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.model.Code.Guess;
 import edu.cnm.deepdive.codebreaker.model.Game;
 import java.security.SecureRandom;
@@ -39,6 +43,25 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     guessList.setAdapter(adapter);
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.new_game:
+        // TODO Start new game.
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
 
   @Override
   public void onClick(View v) {
@@ -46,7 +69,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
       String text = MainActivity.this.guess.getText().toString().toUpperCase();
       Guess guess = game.guess(text);
       adapter.add(guess);
-      MainActivity.this.guess.setText("");
+      guessList.setSelection(adapter.getCount() -1);
+      this.guess.setText("");
     } catch (IllegalArgumentException e) {
       Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
     }
